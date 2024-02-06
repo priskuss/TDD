ecb19014a219c23b8f5d33f60c350420bb8b4f4c
unknown <aniamilton129@gmail.com>
2024-02-06 15:24:21 +0200

    not fully correct, but test is better now

--- test1.cs
+++ test1.cs
@@ -1,44 +1,40 @@
-
-/*double inches = 5;
-double expected = 126;
-double result = ConvertTo(inches, "-mm");
-//5inches to mm
-
-if (expected == result)
-{
-    Console.WriteLine("🟢 Test passed for millimeters");
-}
-else
-{
-    Console.WriteLine("🔴 Test failed for millimeters");
-}
-
-double ConvertTo(double inches, string unit)
-{
-    if (unit == "-mm")
-    {
-        return inches * 25.4;
-    }
-    else
-    {
-        Console.WriteLine("Invalid input");
-        return 0;
-    }
-
-}*/
 Console.WriteLine("Enter a number of inches and a unit to convert to (e.g. 5 -mm):");
-double input = Convert.ToDouble(Console.ReadLine());
-double expected = input * 25.4;
 
-double result = ConvertTo(input, "-mm");
+double inches = Convert.ToDouble(Console.ReadLine());
+double expected = inches * 25.8; // 25.4 correct 
+double resultOne = ConvertTo(inches, "-mm");
 
-if (result == expected)
+if (expected == resultOne)
 {
     Console.WriteLine("🟢 Test passed for millimeters");
 }
 else
 {
     Console.WriteLine("🔴 Test failed for millimeters");
+}
+
+double resultTwo = ConvertTo(inches, "-cm");
+expected = inches * 2.54;
+
+if (expected == resultTwo)
+{
+    Console.WriteLine("🟢 Test passed for centimeters");
+}
+else
+{
+    Console.WriteLine("🔴 Test failed for centimeters");
+}
+
+double resultThree = ConvertTo(inches, "-m");
+expected = inches * 0.0254;
+
+if (expected == resultThree)
+{
+    Console.WriteLine("🟢 Test passed for meters");
+}
+else
+{
+    Console.WriteLine("🔴 Test failed for meters");
 }
 
 double ConvertTo(double inches, string unit)
@@ -61,37 +57,3 @@ double ConvertTo(double inches, string unit)
         return 0;
     }
 }
-/*double expected = ConvertTo(inches, unit);
-
-
-if (expected )
-{
-    Console.WriteLine("🟢 Test passed");
-}
-else
-{
-    Console.WriteLine("🔴 Test failed");
-}
-
-
-?
-double ConvertTo(double inches, string unit)
-{
-    if (unit == "-mm")
-    {
-        return inches * 25.4;
-    }
-    else if (unit == "-cm")
-    {
-        return inches * 2.54;
-    }
-    else if (unit == "-m")
-    {
-        return inches * 0.0254;
-    }
-    else
-    {
-        Console.WriteLine("Invalid input");
-    }
-}
-*/
